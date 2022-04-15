c565c9a222870e65886f8a78aa36a55c10853b8c
xzc0 <1355935992@qq.com>
2022-04-16 01:50:05 +0800

    pocket

--- homework/hw1/PLA.py
+++ homework/hw1/PLA.py
@@ -6,20 +6,21 @@ def PLAClassify(X, W):
     X1 = np.insert(X, X.shape[1], values=1, axis=1)
     return np.sign(np.dot(X1, W))
 
+def PLACheck(X,Y,W):
+    return np.where(Y != PLAClassify(X, W))[0]
 
 def PLATrain(X, Y, rand=False, rate=1, times=100, pocket=True):
     W = np.zeros(X.shape[1]+1)
     idx = -1
     count = 0
-    if rand:
-        X, Y = utils.shuffle(X, Y)
     if pocket:
         rand = True
-        W1 = np.zeros(X.shape[1]+1)
+        PocketW = np.zeros(X.shape[1]+1)
         last_error = X.shape[0]
-
+    if rand:
+        X, Y = utils.shuffle(X, Y)
     while(count <= times):
-        error = np.where(Y != PLAClassify(X, W))[0]
+        error = PLACheck(X,Y,W)
         if error.shape[0] != 0:
             rest = error[error > idx]
             if rest.shape[0] != 0:
@@ -28,10 +29,10 @@ def PLATrain(X, Y, rand=False, rate=1, times=100, pocket=True):
                 idx = -1
                 continue
             if pocket:
-                W1 = W + np.append(X[idx, :],1)*Y[idx]*rate
-                now_error = sum(Y != PLAClassify(X, W1))
+                PocketW = W + np.append(X[idx, :],1)*Y[idx]*rate
+                now_error = PLACheck(X,Y,PocketW).shape[0]
                 if now_error < last_error:
-                    W = W1
+                    W = PocketW
                     last_error = now_error
             else:
                 W += np.append(X[idx, :],1)*Y[idx]*rate
